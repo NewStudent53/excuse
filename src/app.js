9f62d046eaf2548861be6f121cf33039e058955d
Guillermo R <79545062+NewStudent53@users.noreply.github.com>
2024-04-26 01:07:48 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,38 @@
+window.onload = function () {
+
+    let button = document.getElementById('button');
+    button.addEventListener('click', function(){
+        Refresh();
+    });
+    Refresh = () =>{  
+    let number = document.getElementsByClassName('number')[0];
+    let num = Math.floor(Math.random() * 12)+1; //13 lck+++
+    console.log(num);
+    let variable = Math.floor(Math.random() * 4)+1 ;
+    let vari = "";
+    switch (num) {
+        case 1: num = "A";  break;
+        case 10: num = "J"; break;
+        case 11: num = "Q"; break;
+        case 12: num = "K"; break;
+    }
+    switch (variable) {
+        case 1: vari = "heart";    break;
+        case 2: vari = "spade";    break;
+        case 3: vari = "club";     break;
+        case 4: vari = "diamond";  break;
+    }
+    number.innerHTML = num;
+
+    let card = document.getElementsByClassName('card')[0];
+    card.classList = `card ${vari}`;
+}
+ Refresh();
+ };
+
+
 //Reciclando conceptos yes?no?
-function domainName (){
+/*function domainName (){
     let a=0;
     let b=0;
     let c=0;
@@ -27,3 +60,4 @@ document.querySelector("#btn").addEventListener("click", () => {
     document.querySelector("#name").innerHTML = domainName();
 });
 //All done
+*/ 
