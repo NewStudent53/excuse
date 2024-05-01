ef307d3b7a78e21ea9ac090eb497e7f4354deddc
Guillermo R <79545062+NewStudent53@users.noreply.github.com>
2024-05-01 14:39:00 -0400

    Update app.js
    Fixed error

--- src/app.js
+++ src/app.js
@@ -1,35 +1,49 @@
-window.onload = function () {
-
-    let button = document.getElementById('button');
-    button.addEventListener('click', function(){
-        Refresh();
-    });
-    Refresh = () =>{  
-    let number = document.getElementsByClassName('number')[0];
-    let num = Math.floor(Math.random() * 12)+1; //13 lck+++
+var Refresh = (window.onload = function() {
+  let button = document.getElementById("button");
+  button.addEventListener("click", function() {
+    Refresh();
+  });
+  Refresh = () => {
+    let number = document.getElementsByClassName("number")[0];
+    let num = Math.floor(Math.random() * 12) + 1; //13 lck+++
     console.log(num);
-    let variable = Math.floor(Math.random() * 4)+1 ;
+    let variable = Math.floor(Math.random() * 4) + 1;
     let vari = "";
     switch (num) {
-        case 1: num = "A";  break;
-        case 10: num = "J"; break;
-        case 11: num = "Q"; break;
-        case 12: num = "K"; break;
+      case 1:
+        num = "A";
+        break;
+      case 10:
+        num = "J";
+        break;
+      case 11:
+        num = "Q";
+        break;
+      case 12:
+        num = "K";
+        break;
     }
     switch (variable) {
-        case 1: vari = "heart";    break;
-        case 2: vari = "spade";    break;
-        case 3: vari = "club";     break;
-        case 4: vari = "diamond";  break;
+      case 1:
+        vari = "heart";
+        break;
+      case 2:
+        vari = "spade";
+        break;
+      case 3:
+        vari = "club";
+        break;
+      case 4:
+        vari = "diamond";
+        break;
     }
     number.innerHTML = num;
 
-    let card = document.getElementsByClassName('card')[0];
+    let card = document.getElementsByClassName("card")[0];
     card.classList = `card ${vari}`;
-}
- Refresh();
- };
-
+  };
+  Refresh();
+});
 
 //Reciclando conceptos yes?no?
 /*function domainName (){
@@ -60,4 +74,4 @@ document.querySelector("#btn").addEventListener("click", () => {
     document.querySelector("#name").innerHTML = domainName();
 });
 //All done
-*/ 
+*/
